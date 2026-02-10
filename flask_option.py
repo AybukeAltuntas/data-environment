54eedd34ff258bbc7c0e948a98c2fd5d392a082b
Aybuke Altuntas <aybukealtuntas@192.168.1.19>
2026-02-10 15:41:45 +0300

    added docstring

--- flask_option.py
+++ flask_option.py
@@ -1,9 +1,17 @@
-# pylint: disable=missing-docstring
+
 
 import os
 
 def start():
 
+    """
+    Determines the application start mode based on the FLASK_ENV
+    environment variable.
+
+    Returns:
+        str: A message indicating whether the app is running in
+        development mode, production mode, or empty mode.
+    """
     flask_env= os.getenv("FLASK_ENV")
     if flask_env == "development":
         return "Starting in development mode..."
